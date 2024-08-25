784449ff2e92aec0a392ada7ac1a41af2ef47f37
CamyH <cameronhunt88@hotmail.com>
2024-08-25 19:38:26 +0100

    Setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { LandingPage } from './pages/landingPage/LandingPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' />
+        <Route path='/' element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   )
