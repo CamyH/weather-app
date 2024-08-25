be7742e838d461b8025275c6c2c9c321c0ff565a
CamyH <cameronhunt88@hotmail.com>
2024-08-25 19:38:18 +0100

    Add basic scaffolding of landing page

---
+++ src/pages/landingPage/LandingPage.tsx
@@ -0,0 +1,15 @@
+// Components
+
+// Styles
+import { Button } from '@fluentui/react'
+import './landingPage.scss'
+
+export const LandingPage = () => {
+  return (
+    <div id='landing-page-container'>
+      <h1 id='title-heading'>Placeholder</h1>
+      <h2 id='sub-title-heading'>Weather App</h2>
+      <Button></Button>
+    </div>
+  )
+}
