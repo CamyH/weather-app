0afdb1a93f5c9ae789ea5c20330b55cde172e4c1
CamyH <cameronhunt88@hotmail.com>
2025-01-06 17:08:36 +0000

    Remove deprecated button component

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,15 +1,13 @@
 // Components
 
 // Styles
-import { Button } from '@fluentui/react'
-import './landingPage.scss'
+import "./landingPage.scss"
 
 export const LandingPage = () => {
-  return (
-    <div id='landing-page-container'>
-      <h1 id='title-heading'>Placeholder</h1>
-      <h2 id='sub-title-heading'>Weather App</h2>
-      <Button></Button>
-    </div>
-  )
+    return (
+        <div id='landing-page-container'>
+            <h1 id='title-heading'>Placeholder</h1>
+            <h2 id='sub-title-heading'>Weather App</h2>
+        </div>
+    )
 }
